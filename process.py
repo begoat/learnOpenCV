52338cd44e2573422f7d64e2d0f3ff52e3e6f0af
begoat <hylbegoat@gmail.com>
2017-10-13 07:49:14 +0800

    circle

--- process.py
+++ process.py
@@ -8,7 +8,8 @@ def locateMouse(event, x, y ,flags, param):
     if event == cv2.EVENT_MOUSEMOVE:
         mouseX, mouseY = x, y
 
-#cv2.setMouseCallback('success', locateMouse)
+cv2.namedWindow("success")
+cv2.setMouseCallback('success', locateMouse)
 
 # cap = cv2.VideoCapture()
 # cap.open("rtmp://rtmp.open.ys7.com/openlive/3500ecdfeb404eb883c0fe662c5bc297.hd live=1")
@@ -17,7 +18,7 @@ while (1):
     #_, frame = cap.read()
     #img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    img_rgb = cv2.imread('/Users/william/Desktop/chess3.png')
+    img_rgb = cv2.imread('/Users/william/Downloads/123123.png')
     img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
     template = cv2.imread('/Users/william/Pictures/piece2.png', 0)
 
@@ -26,10 +27,13 @@ while (1):
     threshold = 0.4
     loc = np.where( res >= threshold)
     for pt in zip(*loc[::-1]):
-        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+        # cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+        cv2.circle(img_rgb, (int(pt[0]+w/2), int(pt[1]+h/2)), 30, (0,0,255), 2)
     
-    cv2.imshow("test", img_rgb)
+    cv2.imshow("success", img_rgb)
 
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
         break
+    elif k == ord('a'):
+        print (mouseX, mouseY)
